bee7f988bd88df75a741b505dae9d52c22d204b5
Pedro Oliveira <pedrodump21@gmail.com>
2021-08-15 19:57:07 -0300

    Async Tasks

--- WebTest/Services/DepartmentService.cs
+++ WebTest/Services/DepartmentService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using WebTest.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace WebTest.Services
 {
@@ -15,9 +16,9 @@ namespace WebTest.Services
             _context = context;
         }
 
-        public List<Department> FindAll()
+        public async Task<List<Department>> FindAllAsync()
         {
-            return _context.Department.OrderBy(x => x.Name).ToList();
+            return await _context.Department.OrderBy(x => x.Name).ToListAsync();
         }
     }
 }
--- WebTest/Services/SellerService.cs
+++ WebTest/Services/SellerService.cs
@@ -17,40 +17,41 @@ namespace WebTest.Services
             _context = context;
         }
 
-        public List<Seller> FindAll()
+        public async Task<List<Seller>> FindAllAsync()
         {
-            return _context.Seller.ToList();
+            return await _context.Seller.ToListAsync();
             //acessa a fonte de dados de Vendedores
             //e converte para uma lista
         }
 
-        public void Insert(Seller obj)
+        public async Task InsertAsync(Seller obj)
         {
             _context.Add(obj);
-            _context.SaveChanges();
+           await _context.SaveChangesAsync();
         }
-        public Seller FindById(int id)
+        public async Task<Seller> FindByIdAsync(int id)
         {
             //include para dar JOIN de chave estrangeira e mostrar Departamento ID
-            return _context.Seller.Include(obj => obj.Department).FirstOrDefault(obj => obj.Id == id);
+            return await _context.Seller.Include(obj => obj.Department).FirstOrDefaultAsync(obj => obj.Id == id);
         }
-        public void Remove(int id)
+        public async Task RemoveAsync(int id)
         {
-            var obj = _context.Seller.Find(id);
+            var obj = await _context.Seller.FindAsync(id);
             _context.Seller.Remove(obj);
-            _context.SaveChanges(); //remocao de seller
+            await _context.SaveChangesAsync(); //remocao de seller
         }
 
-        public void Update (Seller obj)
+        public async Task UpdateAsync (Seller obj)
         {
-            if (!_context.Seller.Any(x => x.Id == obj.Id)) //existe no BD?
+            bool hasAny = await _context.Seller.AnyAsync(x => x.Id == obj.Id);
+            if (!hasAny) //existe no BD?
             {
                 throw new NotFoundException("Id not found");
             }
             try
             {
                 _context.Update(obj);
-                _context.SaveChanges();
+                await _context.SaveChangesAsync();
             }
             catch (DbUpdateConcurrencyException e)
             {
