fdef13a2eaeb43c7d842e77ed51eeb4c2c8bab16
Pedro Oliveira <pedrodump21@gmail.com>
2021-08-15 18:51:09 -0300

    Formatacao Display Data

--- WebTest/Startup.cs
+++ WebTest/Startup.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
+using System.Globalization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -13,6 +14,7 @@ using Microsoft.EntityFrameworkCore;
 using WebTest.Models;
 using WebTest.Data;
 using WebTest.Services;
+using Microsoft.AspNetCore.Localization;
 
 namespace WebTest
 {
@@ -51,6 +53,16 @@ namespace WebTest
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, SeedingService seedingService)
         {
+            var enUS = new CultureInfo("en-US");
+            var localizationOptions = new RequestLocalizationOptions
+            {
+                DefaultRequestCulture = new RequestCulture(enUS),
+                SupportedCultures = new List<CultureInfo> { enUS },
+                SupportedUICultures = new List<CultureInfo> { enUS }
+            };
+
+            app.UseRequestLocalization(localizationOptions);
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
